8a00434289792855ac35590019036f633b6c5554
soom12 <soo.m@northeastern.edu>
2024-11-08 14:21:54 -0500

    fixes to #3

--- scripts/3_download_protein_fastas.sh
+++ scripts/3_download_protein_fastas.sh
@@ -10,14 +10,14 @@ conda activate /work/geisingerlab/conda_env/blast_corr
 mkdir -p "$FASTA_DIR"
 
 # Parse the file and download each protein FASTA
-awk 'NR > 1 {print $1, $2}' "$METADATA_EXTRACTED_TXT" | while read -r accession organism_name; do
+awk 'NR > 1 && /^GCF/ {print $1, $2}' "$METADATA_EXTRACTED_TXT" | while read -r accession organism_name; do
     # Clean the organism name for use as a filename
     sanitized_name=$(echo "$organism_name" | tr ' ' '_' | tr -d '[:punct:]')
     output_file="${FASTA_DIR}/${sanitized_name}_proteins.faa"
 
     # Download the protein FASTA file for the given accession
     echo "Downloading protein FASTA for $accession ($organism_name)..."
-    datasets download protein genome "$accession" --include fasta --output "${output_file}.zip"
+    datasets download genome accession "$accession" --include protein --filename "${output_file}.zip"
 
     # Unzip the downloaded file and move the protein file to the desired filename
     unzip -o "${output_file}.zip" -d "$FASTA_DIR"
